dcb83e4f3beabcee1100b3534da7ed3f3df8d597
karansaratha@gmail.com <karansaratha@gmail.com>
2021-09-01 18:21:51 +0530

    commit 1/9/21

--- LILAMORENEW/OSA/Helper/GlobalVariables.swift
+++ LILAMORENEW/OSA/Helper/GlobalVariables.swift
@@ -11,27 +11,9 @@ class GlobalVariables: NSObject {
     
     static let shared: GlobalVariables = GlobalVariables()
     
-       var Otp = String()
-       var mobile_Number = String()
-       var categoryList_count = Int()
-       var address_id = String()
-       var birth_date = String()
-       var customer_id = String()
-       var first_name = String()
-       var last_login = String()
-       var last_name = String()
-       var newsletter_status = String()
-       var phone_number = String()
-       var profile_picture = String()
-       var email_Id = String()
-       var total_cart_payment = String()
-       var wallet_amount = String()
-       var order_id = String()
-       var total_price = String()
-       var product_detail_reviewCount = String()
-       var orderCount = Int()
-       var addresslistStatus = String()
-       var wishListCount = Int()
+       var user_loginType = String()
+       var Institute_Code = String()
 }
+
 
 
